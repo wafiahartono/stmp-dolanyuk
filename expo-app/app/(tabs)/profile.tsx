ed58bea810c0dc47e686a95356bd46ef240ab1ab
Wafi Hartono <s160419098@student.ubaya.ac.id>
2024-01-06 02:25:29 +0700

    Update profile UI

--- expo-app/app/(tabs)/profile.tsx
+++ expo-app/app/(tabs)/profile.tsx
@@ -1,9 +1,10 @@
 import * as ImagePicker from "expo-image-picker"
 import { useRouter } from "expo-router"
 import { useCallback, useState } from "react"
-import { ToastAndroid } from "react-native"
-import { Avatar, Button, H4, SizableText, Spinner, YStack } from "tamagui"
+import { Alert, ToastAndroid } from "react-native"
+import { Avatar, Button, Circle, H4, SizableText, Spinner, YStack } from "tamagui"
 
+import { ArrowRightSquare, Image, Trash } from "@tamagui/lucide-icons"
 import { InputField } from "../../components/InputField"
 import { File } from "../../lib/api/File"
 import { InvalidUserError } from "../../lib/api/InvalidUserError"
@@ -115,15 +116,11 @@ export default function Profile() {
 
     updateProfile(params)
       .then(() => {
-        if (updateName) {
-          setName("")
-        }
-
-        if (updatePassword) {
-          setCurrentPassword("")
-          setNewPassword("")
-          setNewPasswordConfirm("")
-        }
+        setPicture(null)
+        setName("")
+        setCurrentPassword("")
+        setNewPassword("")
+        setNewPasswordConfirm("")
 
         ToastAndroid.show("Profile updated.", ToastAndroid.LONG)
       })
@@ -137,8 +134,18 @@ export default function Profile() {
   }, [picture, name, currentPassword, newPassword, newPasswordConfirm])
 
   const handleSignOut = useCallback(() => {
-    signOut()
-    router.replace("/signin")
+    Alert.alert(
+      "Confirm Sign Out",
+      "Are you sure you want to sign out?",
+      [
+        { text: "Cancel" },
+        {
+          text: "Sign Out",
+          onPress: () => signOut()
+            .then(() => router.replace("/signin"))
+        },
+      ],
+    )
   }, [])
 
   return (
@@ -148,11 +155,35 @@ export default function Profile() {
       p="$8"
       bc="$backgroundStrong"
     >
-      <Avatar alignSelf="center" circular size="$8" onPress={handlePickPicture}>
-        <Avatar.Image
-          src="https://images.unsplash.com/photo-1531384441138-2736e62e0919?&w=100&h=100&dpr=2&q=80"
-        />
-      </Avatar>
+      <YStack alignSelf="center">
+        <Avatar
+          size="$10"
+          bw={picture ? "$0.75" : "$0"}
+          boc="$blue8"
+          circular
+        >
+          <Avatar.Image
+            src={
+              picture ? picture.uri : user.picture
+            }
+          />
+        </Avatar>
+
+        <Circle
+          themeInverse
+          pressTheme
+          size="$3"
+          pos="absolute"
+          b={-4}
+          r={-4}
+          bc="$background"
+          onPress={() => picture ? setPicture(null) : handlePickPicture()}
+        >
+          {picture
+            ? <Trash size="$1" color="white" />
+            : <Image size="$1" color="white" />}
+        </Circle>
+      </YStack>
 
       <H4 alignSelf="center" mt="$4">
         {user.name}
@@ -203,13 +234,19 @@ export default function Profile() {
         themeInverse
         disabled={!pendingChanges || updateProfileState.isLoading}
         mt="$4"
+        color={pendingChanges ? "unset" : "$gray10Light"}
+        bc={pendingChanges ? "unset" : "$gray4Light"}
         iconAfter={updateProfileState.isLoading ? <Spinner /> : null}
         onPress={handleUpdateProfile}
       >
         Update Profile
       </Button>
 
-      <Button mt="$4" onPress={handleSignOut}>
+      <Button
+        mt="$4"
+        iconAfter={<ArrowRightSquare size="$1" />}
+        onPress={handleSignOut}
+      >
         Sign Out
       </Button>
     </YStack>
